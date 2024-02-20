27cf124886060277c3bc59af55e0ed44ecfb1ebc
Karen Vasquez <111911263+kvasqu25@users.noreply.github.com>
2024-02-19 20:47:50 -0700

    new

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-function drawTriangle(triangleSize) {
-   for (let i = 1; i <= triangleSize; i++) {
-       let asterisk = '';
-       for (let j = 1; j <= i; j++) {
-         asterisk += '*';
-       }
-       console.log(asterisk);
-   }
-}
-
-drawTriangle(4);
+// Put your solution here
